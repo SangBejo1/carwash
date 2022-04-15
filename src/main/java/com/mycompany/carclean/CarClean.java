b47363d4881487d282ac247cc2af4b393fafb31b
SangBejo1 <ayubsubagiya@gmail.com>
2022-04-15 17:09:15 +0700

    carwash

---
+++ src/main/java/com/mycompany/carclean/CarClean.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.carclean;
+import Frame.carclean;
+/**
+ *
+ * @author HP
+ */
+public class CarClean {
+
+    public static void main(String[] args) {
+       carclean frame = new carclean();
+       frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
